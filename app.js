43c268d546720f456fa6167734460a68e2f69643
Stella Zhao <tmrvivian@gmail.com>
2015-05-16 10:42:49 -0400

    underscore added

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var express = require( 'express' );
 var app = express();
+var swig = require('swig');
 
-//joe testing a change to stella's app.js
+// app.engine (docs) to use swig.renderFile as the function to actually render html
+// app.set (docs) the default view engine to html (so we don't have to specify on every render)
+// app.set (docs) the views path to our views folder (where we store our templates)
+app.engine('html', swig.renderFile);
+app.set('view engine','html');
+app.set('views',__dirname+'/views');
 
+swig.setDefaults({ cache: false });
+
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+
+//Turn off Swig's caching in app.js
 app.get('/', function(req, res){
-	res.send('Hello World');
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
 
-app.get('/stella', function(req, res){
-	res.send('Hello Stella!');
-})
+
 app.listen(3000);
