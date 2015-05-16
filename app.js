8fda9183c1235fc52e670e9f65ee5603b364b289
Stella Zhao <tmrvivian@gmail.com>
2015-05-16 11:37:50 -0400

    got the front page working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require( 'express' );
 var app = express();
 var swig = require('swig');
+var routes = require('./routes/');
 
 // app.engine (docs) to use swig.renderFile as the function to actually render html
 // app.set (docs) the default view engine to html (so we don't have to specify on every render)
@@ -11,13 +12,12 @@ app.set('views',__dirname+'/views');
 
 swig.setDefaults({ cache: false });
 
-var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//The express.static() method takes a root directory parameter and returns a middleware function. Like with Morgan, we pass this middleware to app.use() to intercept all requests. It checks if a request URI path matches a filepath in the public directory; if so, it sends that file back as-is
+app.use(express.static(__dirname + '/public'));
+
+app.use('/',routes);
 
 
-//Turn off Swig's caching in app.js
-app.get('/', function(req, res){
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
-})
 
 
 app.listen(3000);
